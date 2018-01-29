727a87435b8ed82c80926a8bdd4e28662db8edae
Armen-Kazarov <ukraine097@ukr.net>
2018-01-29 19:35:33 +0200

    lesson-3 27.01.2018

---
+++ app/src/components/sidemenu/changeBackground.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ChangeBackground extends React.Component {
+    constructor() {
+        super();
+
+    }
+
+
+    render () {
+        return (
+            <div>
+                ChangeBackground
+            </div>
+        )
+    }
+}
+
+export default ChangeBackground;
---
+++ app/src/components/sidemenu/stickers.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Stickers extends React.Component {
+    constructor() {
+        super();
+
+    }
+
+
+    render () {
+        return (
+            <div>
+               Stickers
+            </div>
+        )
+    }
+}
+
+export default Stickers;
